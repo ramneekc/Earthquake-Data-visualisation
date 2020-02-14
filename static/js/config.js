3a99e5761d644e0265c6990cdefdaf2b49912ac0
ramneekc <ramneekchatha@gmail>
2020-02-13 19:04:36 -0800

    Fixed the Api key

--- static/js/config.js
+++ static/js/config.js
@@ -1,1 +1,2 @@
-const API_KEY = "pk.eyJ1IjoicmFtbmVlayIsImEiOiJjazYxam5ubzAwMXlzM2VxbnNmOG1mN3NhIn0.RNz0i5XPQxsZQOiuiD1qlg"
+// API key
+const API_KEY = "pk.eyJ1IjoicmFtbmVlayIsImEiOiJjazZpaTl4aWMwMWQyM2twaXJla2RteG1tIn0.AsGWaoMxL14ePnx3sNqAlw";
